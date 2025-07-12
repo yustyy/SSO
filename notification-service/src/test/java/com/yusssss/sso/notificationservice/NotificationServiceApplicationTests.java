b62b2225fba3a7bf1bdea0de7c32517678d9a565
yustyy <yusuf.acmaci@hotmail.com>
2025-07-12 22:30:44 +0300

    initialize notification-service

---
+++ notification-service/src/test/java/com/yusssss/sso/notificationservice/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yusssss.sso.notificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
